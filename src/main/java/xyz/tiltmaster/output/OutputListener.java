c00f031f40216d4513ee643613e3543e70166e01
Julian <julianbrendl@gmail.com>
2017-06-04 00:48:04 +0200

    added observer pattern

---
+++ src/main/java/xyz/tiltmaster/output/OutputListener.java
@@ -0,0 +1,11 @@
+package xyz.tiltmaster.output;
+
+import xyz.tiltmaster.util.IEvent;
+import xyz.tiltmaster.util.IListener;
+
+public class OutputListener implements IListener {
+    @Override
+    public void activate(IEvent event) {
+
+    }
+}
