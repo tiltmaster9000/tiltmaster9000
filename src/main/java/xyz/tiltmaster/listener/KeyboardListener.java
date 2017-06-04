092564ce2eb10d478a382af51bab8c222671755c
Julian <julianbrendl@gmail.com>
2017-06-04 15:59:39 +0200

    reduced logging in console

--- src/main/java/xyz/tiltmaster/listener/KeyboardListener.java
+++ src/main/java/xyz/tiltmaster/listener/KeyboardListener.java
@@ -1,16 +1,22 @@
 package xyz.tiltmaster.listener;
 
+import com.tulskiy.keymaster.common.HotKey;
+import com.tulskiy.keymaster.common.HotKeyListener;
+import com.tulskiy.keymaster.common.Provider;
 import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
 import org.jnativehook.keyboard.NativeKeyEvent;
 import org.jnativehook.keyboard.NativeKeyListener;
 import xyz.tiltmaster.util.Observer;
 
+import javax.swing.*;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by David on 04.06.2017.
@@ -28,32 +34,26 @@ public class KeyboardListener extends Observer implements NativeKeyListener {
 
     public void nativeKeyTyped(NativeKeyEvent e) {
         System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
-
     }
 
     public void nativeKeyPressed(NativeKeyEvent e) {
         System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
-
-
     }
-
 
     public static void main(String[] args) {
         properties = new Properties();
 
         BufferedInputStream stream = null;
         try {
-            stream = new BufferedInputStream(new FileInputStream("C:\\Users\\David\\Documents\\Projects\\keymap.properties"));
+            stream = new BufferedInputStream(new FileInputStream("../../keymap.properties"));
             properties.load(stream);
             stream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         try {
             GlobalScreen.registerNativeHook();
-        }
-        catch (NativeHookException ex) {
+        } catch (NativeHookException ex) {
             System.err.println("There was a problem registering the native hook.");
             System.err.println(ex.getMessage());
 
@@ -61,6 +61,8 @@ public class KeyboardListener extends Observer implements NativeKeyListener {
         }
 
         GlobalScreen.addNativeKeyListener(new KeyboardListener());
+        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        logger.setLevel(Level.WARNING);
     }
 
 
