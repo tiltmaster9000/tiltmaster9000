640409a3ff7c62b3f28ab441b59ef19d03fa8058
Julian <julianbrendl@gmail.com>
2017-06-04 18:54:03 +0200

    added atomic boolean

--- src/main/java/xyz/tiltmaster/listener/ActivityListener.java
+++ src/main/java/xyz/tiltmaster/listener/ActivityListener.java
@@ -1,22 +1,24 @@
 package xyz.tiltmaster.listener;
 import xyz.tiltmaster.util.IListener;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 
 public class ActivityListener implements IListener<Boolean> {
-    private boolean isActive = false;
+    private AtomicBoolean isActive = new AtomicBoolean(false);
 
     @Override
     public void activate(Boolean isActive) {
-        this.isActive = isActive;
+        this.isActive.set(isActive);
         System.out.println("Setting typing status to: " + !isActive);
     }
 
     public boolean isActive() {
-        return isActive;
+        return isActive.get();
     }
 
     public void setActive(boolean isActive) {
-        this.isActive = isActive;
+        this.isActive.set(isActive);
         System.out.println("USER ACTION: Setting typing status to: " + !isActive);
     }
 }
