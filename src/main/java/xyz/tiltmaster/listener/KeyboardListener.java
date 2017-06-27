317b1ac0ffd0232b7ab5095a1ec93322e713ac63
Julian <julianbrendl@gmail.com>
2017-06-27 20:48:14 +0200

    renamed tiltfile

--- src/main/java/xyz/tiltmaster/listener/KeyboardListener.java
+++ src/main/java/xyz/tiltmaster/listener/KeyboardListener.java
@@ -34,7 +34,7 @@ public class KeyboardListener extends Notifier<String> implements NativeKeyListe
         BufferedInputStream stream;
         JSONParser parser = new JSONParser();
         try {
-            jsonObjectTemp = (JSONObject) parser.parse(new FileReader("../tiltfile_en.json"));
+            jsonObjectTemp = (JSONObject) parser.parse(new FileReader("../tiltfile.json"));
 
         } catch (IOException | ParseException e) {
             jsonObjectTemp = null;
